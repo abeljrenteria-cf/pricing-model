975059ecac0020d1dee36b69793851bf51e6a94c
abeljrenteria-cf <92360640+abeljrenteria-cf@users.noreply.github.com>
2021-10-12 01:33:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).on('submit','#todo-form',function(e)
+  {
+    console.log('hello');
+    e.preventDefault();
+      $.ajax({
+        type:'POST',
+        url:'/',
+        data:{
+          start_date:$("#start_date").val()
+        },
+        success:function()
+        {
+          alert('saved');
+        }
+      })
+    });
